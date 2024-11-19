2a488d41f8b023b671d4f8b37e77f5ab1e8cde30
sugaroula <139221369+sugaroula@users.noreply.github.com>
2024-11-19 23:21:53 +0100

    Update clj2imp.ml

--- MiniML-Comp/clj2imp.ml
+++ MiniML-Comp/clj2imp.ml
@@ -1,111 +1,97 @@
-(**
-   Translation from Clj to Imp
+open Clj
+open Imp
 
-   To avoid name clashes, the translation generates fresh names on the fly 
-   for every variable.
- *)
-
-(* Module for variables renamings *)
 module STbl = Map.Make(String)
 
-(* Translation of variables
-   - named variables are looked up in the renaming table (or accessed
-     directly)
-   - closure variables generate an array access *)
+let make_struct_env () =
+  let env = Hashtbl.create 10 in
+  let current_id = ref (-1) in
+  fun (s: string) ->
+    match Hashtbl.find_opt env s with
+    | Some id -> id
+    | None ->
+      incr current_id;
+      Hashtbl.add env s !current_id;
+      !current_id
+
+let struct_env = make_struct_env ()
+
 let tr_var v env = match v with
-  | Clj.Name(x) ->
-    Imp.(if STbl.mem x env then Var(STbl.find x env) else Var x)
-      
-  | Clj.CVar(n) ->
-    Imp.(array_get (Var "closure") (Int n))
-      
-(* Translation of an expression
+  | Clj.Name(x) -> Imp.(if STbl.mem x env then Var(STbl.find x env) else Var x)
+  | Clj.CVar(n) -> Imp.(array_get (Var "closure") (Int n))
 
-   Parameters:
-   - e   : Clj expression
-   - env : variable renamings
-
-   Result: triple (s, e', vars)
-   - s    : Imp instruction sequence
-   - e'   : Imp expression
-   - vars : list of fresh variable names introduced for renamings
-   
-   Spec: executing s then evaluating e' in IMP is equivalent to evaluating e in CLJ
-*)
-let tr_expr e env =
-  (* Counter for fresh variable names *)
+let rec tr_expr (e: Clj.expression) (env: string STbl.t):
+    Imp.sequence * Imp.expression * string list =
   let cpt = ref (-1) in
-  (* List of generated names *)
   let vars = ref [] in
-  (* Creation of a variable name of the shape x_N, and recording in vars *)
   let new_var id =
     incr cpt;
     let v = Printf.sprintf "%s_%i" id !cpt in
     vars := v :: !vars;
     v
   in
-  
-  (* Main translation function
-     Return the pair (s, e'), and records variable names in vars as a side effect *)
-  let rec tr_expr (e: Clj.expression) (env: string STbl.t):
+  let rec translate (e: Clj.expression) (env: string STbl.t):
       Imp.sequence * Imp.expression =
     match e with
-      | Clj.Int(n) ->
-         [], Imp.Int(n)
-
-      | Clj.Bool(b) ->
-         [], Imp.Bool(b)
-
-      | Clj.Var(v) ->
-         [], tr_var v env
-          
-      | Clj.Binop(op, e1, e2) ->
-         let is1, te1 = tr_expr e1 env in
-         let is2, te2 = tr_expr e2 env in
-         is1 @ is2, Imp.Binop(op, te1, te2)
-          
-      | Clj.Let(x, e1, e2) ->
-         (* Creation of a unique name for 'x', to be used instead of 'x'
-            in the expression e2. *)
-         let lv = new_var x in
-         let is1, t1 = tr_expr e1 env in
-         let is2, t2 = tr_expr e2 (STbl.add x lv env) in
-         Imp.(is1 @ [Set(lv, t1)] @ is2, t2)
-
-      | _ ->
-         failwith "todo"
-
+    | Clj.Int(n) -> [], Imp.Int(n)
+    | Clj.Bool(b) -> [], Imp.Bool(b)
+    | Clj.Var(v) -> [], tr_var v env
+    | Clj.Binop(op, e1, e2) ->
+      let is1, te1 = translate e1 env in
+      let is2, te2 = translate e2 env in
+      is1 @ is2, Imp.Binop(op, te1, te2)
+    | Clj.Unop(Fst, e1) ->
+      let is1, te1 = translate e1 env in
+      is1, Imp.Deref(Imp.Binop(Add, te1, Imp.Int 4))
+    | Clj.Unop(Snd, e1) ->
+      let is1, te1 = translate e1 env in
+      is1, Imp.Deref(Imp.Binop(Add, te1, Imp.Int 8))
+    | Clj.Unop(op, e1) ->
+      let is1, te1 = translate e1 env in
+      is1, Imp.Unop(op, te1)
+    | Clj.Let(x, e1, e2) ->
+      let lv = new_var x in
+      let is1, t1 = translate e1 env in
+      let is2, t2 = translate e2 (STbl.add x lv env) in
+      Imp.(is1 @ [Set(lv, t1)] @ is2, t2)
+    | Clj.Match(e, cases) ->
+      let is1, te1 = translate e env in
+      let res_var = new_var "match" in
+      let id_var = new_var "case_id" in
+      let is2 = [Imp.Set(id_var, Imp.array_get te1 (Imp.Int 1))] in
+      let rec compile_cases env = function
+        | [] -> []
+        | ((ctor, args), body)::rest ->
+          let matched_id = struct_env ctor in
+          let bind_vars =
+            List.mapi (fun i x -> Imp.Set(x, Imp.array_get te1 (Imp.Int (i + 2)))) args
+          in
+          let is_body, te_body = translate body (List.fold_left (fun env x -> STbl.add x x env) env args) in
+          [Imp.If(Imp.Binop(Eq, Imp.Var id_var, Imp.Int matched_id),
+                 bind_vars @ is_body @ [Imp.Set(res_var, te_body)],
+                 compile_cases env rest)]
+      in
+      is1 @ is2 @ compile_cases env cases, Imp.Var res_var
+    | _ -> failwith "Not yet implemented"
   in
-    
-  let is, te = tr_expr e env in
-  is, te, !vars
+  let seq, expr = translate e env in
+  seq, expr, !vars
 
-    
-(* Translation of a global function *)
 let tr_fdef fdef =
   let env =
     let x = Clj.(fdef.param) in
-    (* The parameter 'x' is renamed 'param_x' *)
     STbl.add x ("param_" ^ x) STbl.empty
   in
-  (* The variables created for the translation of the body of the function
-     are the local variables of the function *)
-  let is, te, locals = tr_expr Clj.(fdef.body) env in
+  let seq, te, locals = tr_expr Clj.(fdef.body) env in
   Imp.({
     name = Clj.(fdef.name);
-    code = is @ [Return te];
-    (* Two parameters: the actual argument, and the closure *)
+    code = seq @ [Return te];
     params = ["param_" ^ Clj.(fdef.param); "closure"];
-    locals = locals;
+    locals;
   })
 
-
-(* Translation of a full program *)
 let translate_program prog =
   let functions = List.map tr_fdef Clj.(prog.functions) in
-  (* Variables of the main expression are the global variables of the program *)
-  let is, te, globals = tr_expr Clj.(prog.code) STbl.empty in
-  (* Main code ends after printing the result of the main expression *)
-  let main = Imp.(is @ [Expr(Call("print_int", [te]))]) in
+  let seq, te, globals = tr_expr Clj.(prog.code) STbl.empty in
+  let main = Imp.(seq @ [Expr(Call("print_int", [te]))]) in
   Imp.({main; functions; globals})
-    

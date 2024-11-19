c70be88081e9c5f9b602e82a06d1f049809e2f08
sugaroula <139221369+sugaroula@users.noreply.github.com>
2024-11-19 23:29:16 +0100

    Update mini2clj.ml

--- MiniML-Comp/mini2clj.ml
+++ MiniML-Comp/mini2clj.ml
@@ -1,95 +1,74 @@
-(**
-   Translation from MiniML to Clj (closure conversion)
- *)
+
+open Miniml
+open Clj
 
 (* Module for sets of variables *)
 module VSet = Set.Make(String)
 
 let translate_program (p: Miniml.prog) =
-  (* List of global function definitions *)
   let fdefs = ref [] in
-  (* Generate a unique function name, for creating a Clj global function
-     from a MiniML anonymous function *)
   let new_fname =
     let cpt = ref (-1) in
     fun () -> incr cpt; Printf.sprintf "fun_%i" !cpt
   in
-  
-  (* Translation of an expression
-     Parameters:
-     - e     : MiniML expression
-     - bvars : set of bound variable names 
-              (bound locally, i.e. not free in the full expression)
 
-     Result: pair (e', l)
-     - e' : Clj expression
-     - l  : association list, maps each free variable to its index in the current closure
-   *)
   let rec tr_expr (e: Miniml.expr) (bvars: VSet.t): Clj.expression * (string * int) list =
-    (* association list, maps free variables to indices in the closure *)
     let cvars = ref [] in
-    (* utility function, to be called for each new free variable:
-       - records the variable in cvars
-       - returns the index *)
     let new_cvar =
-      let cpt = ref 0 in (* indices will start at 1 *)
+      let cpt = ref 0 in
       fun x -> incr cpt; cvars := (x, !cpt) :: !cvars; !cpt
     in
-    
-    (* Translation of a variable, extending the closure if needed *)
+
     let rec convert_var x bvars =
-      Clj.(if VSet.mem x bvars
-        then Name(x) (* if the variable is bound, keep its identifier *)
-        else if List.mem_assoc x !cvars (* if the variable has already been recorded *)
-        then CVar(List.assoc x !cvars) (* then take its index *)
-        else CVar(new_cvar x))         (* otherwise, create and record a new index *)
-        
-    (* Translation of an expression (main loop)
-       Returns the translated expression, and extend the closure as a side effect *)
-    and crawl (e: Miniml.expr) bvars: Clj.expression = match e with
-      | Int(n) ->
-        Int(n)
-
-      | Bool(b) ->
-        Bool(b)
-          
-      | Var(x) ->
-        Var(convert_var x bvars)
-          
-      | Bop(op, e1, e2) ->
-        Binop(op, crawl e1 bvars, crawl e2 bvars)
-
-      (* The range of 'x' in 'let x = e1 in e2' is the expression e2:
-         add x in the bound variables when translating e2 *)
-      | Let(x, e1, e2) ->
-        Let(x, crawl e1 bvars, crawl e2 (VSet.add x bvars))
-
-      (* Translation of an anonymous function *)
-      | Fun(x, _, e) ->
-         (* create a global function definition, and add it to fdefs
-            (this implies creating a new name with new_fname) *)
-         (* return an expression that builds a closure *)
-         failwith "todo"
-
-      | _ ->
-         failwith "todo"
-
+      Clj.(if VSet.mem x bvars then Name(x)
+           else if List.mem_assoc x !cvars then CVar(List.assoc x !cvars)
+           else CVar(new_cvar x))
     in
-    (* Return the result of the translation, and the list of variables to be 
-       put in the closure *)
+
+    let rec crawl (e: Miniml.expr) bvars: Clj.expression =
+      match e with
+      | Int(n) -> Int(n)
+      | Bool(b) -> Bool(b)
+      | Var(x) -> Var(convert_var x bvars)
+      | Bop(op, e1, e2) -> Binop(op, crawl e1 bvars, crawl e2 bvars)
+      | Uop(Fst, e1) -> Unop(Fst, crawl e1 bvars)
+      | Uop(Snd, e1) -> Unop(Snd, crawl e1 bvars)
+      | Uop(op, e1) -> Unop(op, crawl e1 bvars)
+      | Let(x, e1, e2) -> Let(x, crawl e1 bvars, crawl e2 (VSet.add x bvars))
+      | If(e1, e2, e3) -> If(crawl e1 bvars, crawl e2 bvars, crawl e3 bvars)
+      | Fun(x, _, e) ->
+        let fname = new_fname () in
+        let body, vars = tr_expr e (VSet.add x VSet.empty) in
+        let nfdef = Clj.{name = fname; body; param = x} in
+        fdefs := nfdef :: !fdefs;
+        let vl = List.sort (fun (_, i) (_, j) -> compare i j) vars in
+        MkClj(fname, List.map (fun (v, _) -> convert_var v bvars) vl)
+      | App(e1, e2) -> App(crawl e1 bvars, crawl e2 bvars)
+      | Fix(x, _, e) -> Fix(x, crawl e (VSet.add x bvars))
+      | Cstr(ctor, args) -> Cstr(ctor, List.map (fun arg -> crawl arg bvars) args)
+      | Match(e, cases) ->
+        let check_no_duplicate_vars pattern =
+          let rec collect_vars (vars, seen) = function
+            | [] -> vars, seen
+            | x::xs ->
+              if List.mem x seen then failwith "Duplicate variable in pattern"
+              else collect_vars (x::vars, x::seen) xs
+          in
+          collect_vars ([], []) pattern
+        in
+        Match(crawl e bvars,
+              List.map (fun ((ctor, vars), expr) ->
+                let vars, _ = check_no_duplicate_vars vars in
+                (ctor, vars), crawl expr (List.fold_right VSet.add vars bvars))
+              cases)
+    in
     let te = crawl e bvars in
     te, !cvars
-
   in
 
-  (* Translation of a full program:
-     - start with an empty set of bound variables
-     - collect the translated main expression and the global function definitions *)
-  (* Remark: for the main expression, the set of free variables collected in cvars
-     shall be empty (otherwise, the program has undefined variables!) *)
   let code, cvars = tr_expr p.code VSet.empty in
   assert (cvars = []);
   Clj.({
     functions = !fdefs;
-    code = code;
+    code;
   })

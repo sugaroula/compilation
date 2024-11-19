a92a1afdf48aaee95d29e028f4ccc1fe10840afc
sugaroula <139221369+sugaroula@users.noreply.github.com>
2024-11-19 23:47:37 +0100

    Update linearscan.ml

--- ImpOpt/linearscan.ml
+++ ImpOpt/linearscan.ml
@@ -1,48 +1,46 @@
+
 open Imp
 open Nimp
 
-(* sort by ascending lower bound, and sort equals by ascending upper bound *)
-let sort_2 l =
-  List.stable_sort (fun (_, l1, _) (_, l2, _) -> l1 - l2) l
-let sort_3 l =
-  List.stable_sort (fun (_, _, h1) (_, _, h2) -> h1 - h2) l
-let sort_intervals l =
-  sort_2 (sort_3 l)
+(* Sort intervals by start time and end time *)
+let sort_intervals intervals =
+  List.sort (fun (_, start1, _) (_, start2, _) -> compare start1 start2)
+    (List.sort (fun (_, _, end1) (_, _, end2) -> compare end1 end2) intervals)
 
-(* insert interval [i] in active list [l] 
-   pre/post-condition: sorted by ascending upper bound *)
-let rec insert_active i l =
-  failwith "not implemented"
+(* Insert interval into active list maintaining order by end time *)
+let rec insert_active interval active =
+  match active with
+  | [] -> [interval]
+  | i :: rest ->
+    let (_, _, end_i) = i in
+    let (_, _, end_interval) = interval in
+    if end_i < end_interval then i :: insert_active interval rest
+    else interval :: active
 
-(* raw allocation information for a variable *)
-type raw_alloc =
-  | RegN  of int  (* index of the register *)
-  | Spill of int  (* index of the spill *)
-
-(* allocation of the local variables of a function [fdef] using linear scan
-   algorithm, with [nb_regs] registers available for allocation ;
-   return a raw allocation for each variable, as well as the maximum index of
-   used registers, and the number of used stack slots (spills) *)
+(* Linear scan register allocation *)
 let lscan_alloc nb_regs fdef =
   let live_intervals = Liveness.liveness_intervals_from_liveness fdef in
   let alloc = Hashtbl.create (List.length fdef.locals) in
   let active = ref [] in
   let free = ref (List.init nb_regs (fun i -> i)) in
-  let r_max = ref (-1) in (* maximum index of used register *)
-  let spill_count = ref 0 in (* number of spilled variables *)
-  (* free registers allocated to intervals that stop before timestamp a,
-     returns remaining intervals *)
-  let rec expire a l =
-    failwith "not implemented"
+  let r_max = ref (-1) in
+  let spill_count = ref 0 in
+
+  let rec expire_old_intervals timestamp =
+    active := List.filter (fun (_, _, end_time) -> end_time >= timestamp) !active
   in
-  (* for each interval i, in sorted order *)
-  List.iter (fun i ->
-      let xi, li, hi = i in
-      (* free registers that expire before the lower bound of i *)
-      (* if there are available registers *)
-        (* ... then allocate one *)
-        (* otherwise, may replace an already used register if this can
-           make this register available again earlier *)
-      failwith "not implemented"
+
+  List.iter (fun (var, start_time, end_time) ->
+      expire_old_intervals start_time;
+
+      if !free <> [] then
+        let reg = List.hd !free in
+        free := List.tl !free;
+        r_max := max !r_max reg;
+        Hashtbl.add alloc var reg;
+        active := insert_active (var, start_time, end_time) !active
+      else
+        (Hashtbl.add alloc var (-1); incr spill_count)
     ) (sort_intervals live_intervals);
+
   alloc, !r_max, !spill_count
